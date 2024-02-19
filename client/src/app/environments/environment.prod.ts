c635ff6d7976cdc0dec7dbc9b793907a963ed7be
ktruon01 <kevintruong.11235@gmail.com>
2024-02-19 17:52:18 -0500

    Build

--- client/src/app/environments/environment.prod.ts
+++ client/src/app/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 // environment.prod.ts
-export const environment = {
-  production: true,
-  apiUrl: 'https://study-together.onrender.com/api' // Example API URL for production
-};
 // export const environment = {
 //   production: true,
-//   apiUrl: 'https://localhost:3000/api' // Example API URL for development
+//   apiUrl: 'https://study-together.onrender.com/api' // Example API URL for production
 // };
+export const environment = {
+  production: true,
+  apiUrl: 'https://localhost:3000/api' // Example API URL for development
+};
