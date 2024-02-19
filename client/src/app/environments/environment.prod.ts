6682d0126479e3f526c0b4427b4193a4aecf5bfa
ktruon01 <kevintruong.11235@gmail.com>
2024-02-19 17:18:41 -0500

    Build

--- client/src/app/environments/environment.prod.ts
+++ client/src/app/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 // environment.prod.ts
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://study-together.onrender.com/api' // Example API URL for production
+// };
 export const environment = {
   production: true,
-  apiUrl: 'https://study-together.onrender.com/api' // Example API URL for production
+  apiUrl: 'http://localhost:3000/api' // Example API URL for development
 };
